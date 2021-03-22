f5d5cea24cf824b5b38919d2839be8bbc33b8650
José Paumard <Jose.Paumard@gmail.com>
2021-03-22 15:12:20 +0100

    Added files for stage 1

---
+++ src/org/paumard/elevator/student/DumbElevator.java
@@ -0,0 +1,49 @@
+package org.paumard.elevator.student;
+
+import org.paumard.elevator.Elevator;
+import org.paumard.elevator.model.Person;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class DumbElevator implements Elevator {
+
+    private int elevatorCapacity;
+
+    public DumbElevator(int elevatorCapacity) {
+        this.elevatorCapacity = elevatorCapacity;
+    }
+
+    @Override
+    public void startsAtFloor(LocalTime time, int initialFloor) {
+    }
+
+    @Override
+    public void peopleWaiting(List<List<Person>> peopleByFloor) {
+    }
+
+    @Override
+    public int chooseNextFloor() {
+        return 0;
+    }
+
+    @Override
+    public void arriveAtFloor(int floor) {
+    }
+
+    @Override
+    public void loadPerson(Person person) {
+    }
+
+    @Override
+    public void unloadPerson(Person person) {
+    }
+
+    @Override
+    public void newPersonWaitingAtFloor(int floor, Person person) {
+    }
+
+    @Override
+    public void lastPersonArrived() {
+    }
+}
